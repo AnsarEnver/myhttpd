82766e54992076b630116c65d77245ac707a5849
Ansar.Enver <ansar_enver@outlook.com>
2022-07-30 20:56:38 +0800

    The first commit.

--- src/resource.cpp
+++ src/resource.cpp
@@ -1,3 +1,5 @@
+#include <boost/filesystem.hpp>
+
 #include "mime_types.h"
 #include "resource.h"
 
@@ -6,4 +8,13 @@ resource::resource(std::string root, std::map<std::string, std::string> table)
     for(int i = 0; i < sizeof(mime_types) / sizeof(_mime_types[0]); i++){
         this->_mime_types.insert({mime_types[i][0], mime_types[i][1]});
     }
+    if(root[root.size() - 1] == '\\' || root[root.size() - 1] == '/'){
+        this->_root = root.substr(root.size() - 1);
+    }else{
+        this->_root = root;
+    }
+}
+
+bool resource::exists(std::string url){
+    return boost::filesystem::exists(this->_root + url);
 }
